c395a49e6a403c5fb616c2440f22af3c455518e8
Denis Mishankov <mishankov@mail.com>
2024-10-12 16:16:49 +0300

    Update keys.ts

--- packages/excalidraw/keys.ts
+++ packages/excalidraw/keys.ts
@@ -87,7 +87,7 @@ export const isArrowKey = (key: string) =>
   key === KEYS.ARROW_LEFT ||
   key === KEYS.ARROW_RIGHT ||
   key === KEYS.ARROW_DOWN ||
-  key === KEYS.ARROW_UP;\
+  key === KEYS.ARROW_UP;
 
 export const shouldResizeFromCenter = (event: MouseEvent | KeyboardEvent) =>
   event.altKey;
